2a4f737c038905cc13e7ce5d47a653c21e31e357
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-31 23:19:25 +0000

    Commit # 0: format_selector.c holberton.h man_3_printf _putchar.c Added

---
+++ format_selector.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * format_selector - function that selects the format to be
+ * applied to the input parameters.
+ * @format: string that contains the format to be applied by printf.
+ * @list: list of arguments
+ *
+ * Return: numbers of characters printed if successful.
+ */
+
+int format_selector(char format, va_list list)
+{
+
+	int sum = 0;
+	char *string;
+	int num = 0;
+	char c;
+
+	switch (format)
+	{
+		case 'c':
+		c = va_arg(list, int);
+		_putchar (c);
+		sum = 1;
+		break;
+		case 's':
+		string = va_arg(list, char *);
+		if (string == NULL)
+		{
+		_puts("(null)");
+		sum = 6;
+		break;
+		}
+		_puts(string);
+		sum = _strlen(string);		
+		break;
+		case 'd':
+		case 'i':
+		num = va_arg(list, int);
+		print_number(num);
+		sum = _digits(num);
+		break;
+		case '%':
+		_putchar('%');
+		sum = 1;
+		break;
+		default:
+		if (format == '\0')
+		{
+		_putchar('%');
+		_putchar(format);
+		sum = 2;
+		break;
+		}
+	}
+	return (sum);
+}
---
+++ holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int format_selector(char format, va_list list);
+int _printf(const char *format, ...);
+int _putchar(char c);
+void _puts(char *str);
+void print_number(int n);
+int _strlen(char *s);
+int _digits(int num);
+
+#endif
